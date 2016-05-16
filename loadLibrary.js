e23f5ed55328c3fae51786e7b5ab96307bb77b9e
JohnSpencerTerry <John.Spencer.Terry@gmail.com>
2016-05-16 13:23:06 -0400

    created autoloader

--- loadLibrary.js
+++ loadLibrary.js
@@ -1,6 +1,30 @@
-var fs = require('fs');
+// kethinov/walksync.js
 
-fs.readdir('./lib_', function(err, files){
-    if(err) throw err;
-    console.log(files);
-});
+var walkSync = function(dir, filelist) {
+  var fs = fs || require('fs'),
+      files = fs.readdirSync(dir);
+  filelist = filelist || [];
+  files.forEach(function(file) {
+    if (fs.statSync(dir + file).isDirectory()) {
+      filelist = walkSync(dir + file + '/', filelist);
+    }
+    else {
+      filelist.push(file);
+    }
+  });
+  return filelist;
+};
+
+var formatAsImport = function(filelist){
+    filelist.forEach(function(file){
+        file = "@import " + file;
+    });
+    return filelist;
+};
+
+var importedLessFiles =[];
+var rootDirectory = "./lib_/";
+
+walkSync(rootDirectory, importedLessFiles);
+console.log(formatAsImport(importedLessFiles));
+console.log(importedLessFiles);
